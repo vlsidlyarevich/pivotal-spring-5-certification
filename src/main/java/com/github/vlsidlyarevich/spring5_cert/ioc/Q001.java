81be794cee6c22a980b49b9a641cc815160a6329
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2019-10-05 20:23:41 +0300

    Javadoc plugin added.

---
+++ src/main/java/com/github/vlsidlyarevich/spring5_cert/ioc/Q001.java
@@ -0,0 +1,11 @@
+package com.github.vlsidlyarevich.spring5_cert.ioc;
+
+/**
+ * <b>What is dependency injection and what are the advantages?</b>
+ * <br><br>
+ * Answer here
+ *
+ * @author vlsidlyarevich
+ */
+public class Q001 {
+}
