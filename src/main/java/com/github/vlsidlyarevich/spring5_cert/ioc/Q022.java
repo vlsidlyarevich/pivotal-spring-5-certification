2e1276d449d1df976404701585041864968aa840
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2019-10-31 20:14:43 +0300

    Questions 22-23 are added.

---
+++ src/main/java/com/github/vlsidlyarevich/spring5_cert/ioc/Q022.java
@@ -0,0 +1,10 @@
+package com.github.vlsidlyarevich.spring5_cert.ioc;
+
+/**
+ * <b>Can you use @Bean together with {@code @Profile?}</b>
+ * <br><br>
+ *
+ * @author vlsidlyarevich
+ */
+public class Q022 {
+}
---
+++ src/main/java/com/github/vlsidlyarevich/spring5_cert/ioc/Q023.java
@@ -0,0 +1,10 @@
+package com.github.vlsidlyarevich.spring5_cert.ioc;
+
+/**
+ * <b>Can you use @Component together with @Profile?</b>
+ * <br><br>
+ *
+ * @author vlsidlyarevich
+ */
+public class Q023 {
+}
